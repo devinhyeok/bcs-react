64372f53d3437bfbedb52e80b550e9831626b091
devinhyeok <devinhyeok@gmail.com>
2024-12-03 14:40:20 +0900

    fix-calculator

---
+++ pratice/calculator-pratice/script.js
@@ -0,0 +1,20 @@
+function calculate() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const operation = document.getElementById("operation").value;
+  let result;
+
+  if (operation === "add") {
+    result = num1 + num2;
+  } else if (operation === "subtract") {
+    result = num1 - num2;
+  } else if (operation === "multiply") {
+    result = num1 * num2;
+  } else if (operation === "divide") {
+    result = num1 / num2;
+  } else {
+    result = "Invalid operation!";
+  }
+
+  document.getElementById("result").textContent = "Result: " + result;
+}
